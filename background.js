d1772ce070209d833bc47fc57ef9a9ed97517e5d
mursel <murselmusabasic@gmail.com>
2012-02-27 12:34:33 +0100

    new file:   README 	new file:   background.htm 	new file:   background.js 	new file:   icon.png new file:   manifest.json

---
+++ background.js
@@ -0,0 +1,19 @@
+﻿chrome.browserAction.onClicked.addListener(sendToUrl);
+
+chrome.contextMenus.create({
+	"title": "Send link to email",
+	"onclick": OnContextMenuClick,
+	"contexts": ["all"]
+});
+
+function OnContextMenuClick(info, tab)
+{
+	sendToUrl(tab);
+}
+
+function sendToUrl(tab)
+{
+	chrome.tabs.create({
+		"url": "mailto:murselmusabasic@gmail.com?subject="+tab.title+"&body="+tab.url
+	});
+}
